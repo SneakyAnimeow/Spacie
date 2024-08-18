997a53b59f917eabb0f9ad9329374b7e571e5d58
SneakyAnimeow <sneakyanimeow@anims.club>
2024-08-18 16:04:09 +0200

    Add TestCommand to handle 'test' slash command
    This commit introduces the TestCommand class and ITestCommand interface in the App.Discord.Commands.TestCommand namespace. The TestCommand processes 'test' slash commands by collecting 'name' and 'value' options and adding them to the CustomValue repository. The 'name' parameter is required for successful command execution.

---
+++ App/Discord/Commands/TestCommand/ITestCommand.cs
@@ -0,0 +1,8 @@
+﻿using Discord;
+
+namespace App.Discord.Commands.TestCommand;
+
+[SlashCommand("test", "Test command")]
+[SlashCommandOption("name", "Name of the test", ApplicationCommandOptionType.String, true)]
+[SlashCommandOption("value", "Value of the test", ApplicationCommandOptionType.String, false)]
+public interface ITestCommand : IBaseSlashCommand;
---
+++ App/Discord/Commands/TestCommand/TestCommand.cs
@@ -0,0 +1,38 @@
+﻿using App.Data.Sqlite.Entities;
+using App.Data.Sqlite.Repositories.CustomValueRepository;
+using Discord.WebSocket;
+
+namespace App.Discord.Commands.TestCommand;
+
+public class TestCommand : ITestCommand
+{
+    private readonly ICustomValueRepository _customValueRepository;
+    
+    public TestCommand(ICustomValueRepository customValueRepository)
+    {
+        _customValueRepository = customValueRepository;
+    }
+    
+    public async Task HandleAsync(SocketSlashCommand command)
+    {
+        var name = command.Data.Options.FirstOrDefault(x => x.Name == "name")?.Value as string;
+        
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            await command.RespondAsync("Name is required");
+            return;
+        }
+        
+        var value = command.Data.Options.FirstOrDefault(x => x.Name == "value")?.Value as string;
+        
+        var test = new CustomValue
+        {
+            Name = name,
+            Value = value
+        };
+        
+        await _customValueRepository.AddAsync(test);
+        
+        await command.RespondAsync("Test added");
+    }
+}
